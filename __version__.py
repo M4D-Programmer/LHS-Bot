ddf605d882cc589f125d18188d6198c623c7419f
M4D-Programmer <55305244+M4D-Programmer@users.noreply.github.com>
2021-11-09 00:28:14 -0500

    Create __version__.py

---
+++ __version__.py
@@ -0,0 +1,13 @@
+author = "M4D-Programmer"
+version = "0.1.0"
+
+Msg = f""
+
+Cogs = ["8Ball", "AdminCommands", 
+        "Basic", "Bot-to-Bot", 
+        "Censor", "CoinFlip", 
+        "Math", "Music", 
+        "Open_Date", "Polls", 
+        "Roles", "TicTacToe", 
+        "Verification", "Vocab_Definer", 
+        "WheelOfOptions"]
